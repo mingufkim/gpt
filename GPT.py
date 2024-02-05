8a16762c3e1a1fe93a361e1faab6676ff96b483c
Mingu Kim <mingufkim@gmail.com>
2024-02-06 05:18:18 +0900

    Index page

---
+++ GPT.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.set_page_config(page_title="GPT", page_icon="🤖")
+
+st.markdown(
+    """
+    # GPT
+    
+    ---
+    ## Introduction
+    Generative pre-trained transformers (GPT) are a type of large language model (LLM) and a prominent framework for generative artificial intelligence. 
+    They are artificial neural networks that are used in natural language processing tasks. 
+    GPTs are based on the transformer architecture, pre-trained on large data sets of unlabelled text, and able to generate novel human-like content. 
+    As of 2024, most LLMs have these characteristics and are sometimes referred to broadly as GPTs.
+    
+    ---
+    ## Apps
+    - [ ] [DocsGPT](/DocsGPT)
+    
+    ---
+    ## Tools
+    - Python
+    - Streamlit
+    
+    """
+)
