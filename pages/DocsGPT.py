34fcefeb036f631eca5be5ada8486018912c4efe
Mingu Kim <mingufkim@gmail.com>
2024-02-07 03:58:18 +0900

    Implemented document-based QA feature using OpenAI

--- pages/DocsGPT.py
+++ pages/DocsGPT.py
@@ -3,9 +3,11 @@ import streamlit as st
 from langchain.storage import LocalFileStore
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import UnstructuredFileLoader
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.embeddings import CacheBackedEmbeddings
 from langchain_community.vectorstores.faiss import FAISS
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 
 st.set_page_config(page_title="DocsGPT", page_icon="📜")
 
@@ -59,12 +61,49 @@ def display_message_history():
         send_message(message["message"], message["role"], save=False)
 
 
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Answer the question only the following documents. 
+            If you don't know the answer, just say "I don't know". 
+            Don't make up an answer.
+            
+            Documents: {documents}
+            """,
+        ),
+        ("human", "{question}"),
+    ]
+)
+
+llm = ChatOpenAI(
+    temperature=0.5,
+)
+
+
 if file:
     retriever = embed_file(file)
-    send_message("If you have any questions about docs, feel free to ask", "ai", save=False)
+    send_message(
+        "If you have any questions about docs, feel free to ask", "ai", save=False
+    )
     display_message_history()
     message = st.chat_input("Ask a question")
     if message:
         send_message(message, "human")
+        chain = (
+            {
+                "documents": retriever | RunnableLambda(format_docs),
+                "question": RunnablePassthrough(),
+            }
+            | prompt
+            | llm
+        )
+        res = chain.invoke(message)
+        send_message(res.content, "ai")
 else:
     st.session_state["messages"] = []
