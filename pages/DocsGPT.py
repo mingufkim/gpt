086a8e8f09de39e16d592a4d57ec2bfd0f05f105
Mingu Kim <mingufkim@gmail.com>
2024-02-06 20:53:29 +0900

    Add functionality to retrieve information from uploaded docs using embeddings

--- pages/DocsGPT.py
+++ pages/DocsGPT.py
@@ -1,32 +1,47 @@
 import streamlit as st
-import time
 
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import UnstructuredFileLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain.embeddings import CacheBackedEmbeddings
+from langchain_community.vectorstores.faiss import FAISS
 
 st.set_page_config(page_title="DocsGPT", page_icon="📜")
 
-st.title("DocsGPT")
+st.markdown(
+    """
+    # DocsGPT
+    Use this app to ask questions to the DocsGPT about documents.
+    
+    ---
+    """
+)
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
-
-
-def send_message(message, role, save=True):
-    with st.chat_message(role):
-        st.write(message)
-    if save:
-        st.session_state["messages"].append({"message": message, "role": role})
-
-
-for message in st.session_state["messages"]:
-    send_message(message["message"], message["role"], save=False)
+file = st.file_uploader("Upload a file", type=["pdf", "txt", "docx"])
 
 
-message = st.chat_input("Type a message...")
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n\n",
+        chunk_size=512,
+        chunk_overlap=128,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
 
-if message:
-    send_message(message, "human")
-    time.sleep(2)
-    send_message(f"You said: {message}", "ai")
 
-    with st.sidebar:
-        st.write(st.session_state)
+if file:
+    retriever = embed_file(file)
+    r = retriever.invoke("Who is Winston?")
+    r
